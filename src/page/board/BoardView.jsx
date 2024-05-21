853f559a91e062dc5c4ac0743f6c003e86b3a1f8
anwoomin <askcw39@naver.com>
2024-05-21 15:12:06 +0900

    한 게시물 보기

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,12 +1,52 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
+  const [board, setBoard] = useState(null);
   useEffect(() => {
-    axios.get(`/api/board/${id}`);
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
-  return null;
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <Box>{board.id}번 게시물</Box>
+      <Box>
+        <FormControl>
+          <FormLabel>
+            <Input value={board.title} readOnly />
+          </FormLabel>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea value={board.content} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input value={board.writer} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>작성일시</FormControl>
+        <Input type={"datetime-local"} value={board.inserted} readOnly />
+      </Box>
+    </Box>
+  );
 }
