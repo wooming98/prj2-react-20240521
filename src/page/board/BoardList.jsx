5e520c9b467cb1079deb59422a87ac624a241683
anwoomin <askcw39@naver.com>
2024-05-21 14:28:23 +0900

    게시물 목록 보기 화면

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,14 +1,15 @@
 import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
 
-  // [{id:5, title: "제목1", writer: "누구1"},
-  // {id:5, title: "제목1", writer: "누구1"},
-  // {id:5, title: "제목1", writer: "누구1"}]
+  useEffect(() => {
+    axios.get("/api/board/list").then((res) => setBoardList(res.data));
+  }, []);
 
   return (
     <Box>
