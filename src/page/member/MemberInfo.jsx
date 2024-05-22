ecd9ae870caca6f4a27ba494a4459633b0cffe8f
anwoomin <askcw39@naver.com>
2024-05-22 15:18:28 +0900

    회원 정보 보기

---
+++ src/page/member/MemberInfo.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+
+export function MemberInfo() {
+  return (
+    <Box>
+      <Box>회원 정보</Box>
+      <Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+}
