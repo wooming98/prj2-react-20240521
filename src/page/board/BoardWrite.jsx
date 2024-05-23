1e22354f6be08c46ed83bb9abe9438be5bad84c9
anwoomin <askcw39@naver.com>
2024-05-23 16:09:28 +0900

    글 작성시 제목, 본문만 전송

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -56,9 +56,6 @@ export function BoardWrite() {
   if (content.trim().length === 0) {
     disableSaveButton = true;
   }
-  if (writer.trim().length === 0) {
-    disableSaveButton = true;
-  }
 
   return (
     <Box>
