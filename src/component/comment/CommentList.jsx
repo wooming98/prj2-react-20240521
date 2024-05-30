19d3bfdb4f690aca48c77dfb4d6650ea87be3228
anwoomin <askcw39@naver.com>
2024-05-30 11:45:55 +0900

    댓글 목록

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -1,3 +1,33 @@
-export function CommentList() {
-  return null;
+import { useEffect, useState } from "react";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+import axios from "axios";
+
+export function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/api/comment/list/${boardId}`)
+      .then((res) => {
+        setCommentList(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {});
+  }, []);
+  if (commentList.length === 0) {
+    return <Box>댓글이 없습니다. 첫 댓글을 작성해보세요.</Box>;
+  }
+  return (
+    <Box>
+      {commentList.map((comment) => (
+        <Box key={comment.id} border={"1px solid black"} my={3}>
+          <Flex>
+            <Box>{comment.memberId}</Box>
+            <Spacer />
+            <Box>{comment.inserted}</Box>
+          </Flex>
+          <Box>{comment.comment}</Box>
+        </Box>
+      ))}
+    </Box>
+  );
 }
